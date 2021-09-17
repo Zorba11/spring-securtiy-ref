fb12a9510780de413b51227aa8eb37fcf2ed6881
Zorba11 <zorbathegreek111@gmail.com>
2021-09-17 11:35:45 -0600

    boiler-plate

---
+++ src/main/java/com/zorba11/springsecurityref/SpringSecurityRefApplication.java
@@ -0,0 +1,13 @@
+package com.zorba11.springsecurityref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zorba11/springsecurityref/SpringSecurityRefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zorba11.springsecurityref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
